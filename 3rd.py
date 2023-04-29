b9380b3b8fe85a009bc01af9301f6df31008137f
Ayush0095 <132159520+Ayush0095@users.noreply.github.com>
2023-04-30 00:06:38 +0530

    Add files via upload
    OTP generator.

---
+++ 3rd.py
@@ -0,0 +1,11 @@
+import random
+
+print('\n\n______________________________OTP Generator____________________________\n\n')
+
+W= random.randint(1000,9999)
+
+print("\t\t__________________Your 4 digit OTP is_______________:",W)
+
+W= random.randint(100000,999999)
+
+print("\n\t\t_____________________Your 6 digit OTP is________________:",W)
